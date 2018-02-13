0abd2752c1e5dbcac3766010e6188eea5a7efcfb
Felipe Duarte <felipelageduarte@gmail.com>
2018-02-13 20:32:45 -0200

    tunning parameter in regression notebooks. Same must be done over classification notebooks

--- R/utils.r
+++ R/utils.r
@@ -1,3 +1,26 @@
+library(tools, warn.conflicts = FALSE, quietly = T)
+library(data.table, warn.conflicts = FALSE, quietly = T)
+library(mlr, warn.conflicts = FALSE, quietly = T)
+
+#String Concat
+`%++%` <- function(a, b) paste0(a, b)
+`%+ %` <- function(a, b) paste(a, b)
+`%+/%` <- function(a, b) paste(a, b, sep='/')
+`%+_%` <- function(a, b) paste(a, b, sep='_')   
+
+#leitura dados
+read_data <- function(file_path, ...){
+    ext = file_ext(file_path)
+    if(ext == 'csv'){
+        return(as.data.frame(data.table::fread(input = file_path, stringsAsFactors = TRUE, showProgress = TRUE, ...)))
+    } else if(ext == 'dat'){
+        return(as.data.frame(data.table::fread(input = file_path, stringsAsFactors = TRUE, showProgress = TRUE, ...)))
+    } else {
+        stop(sprintf('Uknow file extension.'))
+    }
+}
+
+#Cria métrica Gini
 gini = makeMeasure(
     id = "gini", name = "Gini",
     properties = c("classif", "req.pred", "req.truth", "re.prob"),
